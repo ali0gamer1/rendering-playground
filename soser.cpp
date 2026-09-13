652d49f5c1129effd29c66bacdeb8b7ef3e2260f
Ali <gta7iv7@gmail.com>
2026-09-13 14:18:09 +0330

    Add SDL libraries as submodules

--- soser.cpp
+++ soser.cpp
@@ -1,6 +1,26 @@
 // main.cpp
 #include <iostream>
+#include <chrono>
+#include <thread>
 
-int main() {
-    std::cout << "Hedllo!\n";
+
+
+int main(){
+  using namespace std::chrono;
+
+  time_point StartTime{
+    system_clock::now()
+  };
+
+  std::this_thread::sleep_for(seconds(2));
+
+  time_point EndTime{
+    system_clock::now()
+  };
+
+  duration RunningTime{
+    EndTime - StartTime
+  };
+
+
 }
